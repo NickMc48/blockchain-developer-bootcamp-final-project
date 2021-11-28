bdea775cd4d64ba804a2ffbb8fff3363fbe582b3
NickMc48 <nkmc48@gmail.com>
2021-11-28 11:36:27 -0700

    Deploy to Rinkeby and clean up README

--- README.md
+++ README.md
@@ -21,17 +21,91 @@ test
    Contains files required to run all local tests for this project.
 
 
-## Public Ethereum Account
+## Public Ethereum Account for NFT
 ```
 0x881B7cF77e36BF15164FBD35be71B174FD2621bE
 ```
 
 ## Front-end location
+This was developed using HTML and Javascript (I am sure it wil be ovbious, but this is my first web 'app', sorry.)
 ```
 URL: http://NickMc48.github.io
 Repository: https://github.com/NickMc48/NickMc48.github.io
 ```
 
-## Installing Dependencies
+## Contract Location
+The contract is located on the Rinkeby test network at address:
+```
+0x9Ff7f6d8D8b1Cd70669c11a9D17E327F8268b61D
+```
 
-## 
+## Installing Dependencies
+Run the following commands to install dependencies needed for this project:
+
+Install MetaMask in your browser: [MetaMask.io](https://metamask.io/download.html)
+
+Install Node.js: [Nodejs.org](https://nodejs.org/en/download/package-manager/)
+
+Then install each of the following:
+
+```
+npm install -g npm
+```
+
+```
+npm install -g solc@0.8.10
+```
+
+```
+npm install @openzeppelin/contracts
+```
+
+```
+npm install -g truffle
+```
+```
+npm install -g ganache-cli
+```
+```
+npm install  @truffle/hdwallet-provider
+```
+
+```
+npm install dotenv
+```
+
+## Unit Testing Procedure
+The unit tests are contained in the file at: *./test/discrepancy_report_manager.test.js*
+
+In '*truffle-config.js*', set the **Development** port to **9545**:
+```
+  networks: {
+    development: {
+     host: "127.0.0.1",     // Localhost (default: none)
+     port: 9545,            // Standard Ethereum port (default: none)
+     network_id: "*",       // Any network (default: none)
+    },
+ ...
+```
+
+Set the exact compiler version to **0.8.10** as specified in the contract:
+```
+  // Configure your compilers
+  compilers: {
+    solc: {
+      version: "0.8.10", 
+    }
+  },
+```
+From the project root folder, run:
+```
+truffle develop
+```
+
+Then run:
+```
+test --network development
+```
+
+## Final Project Walk-through Recording
+A recording of my final walkthrough will be here: 
--- truffle-config.js
+++ truffle-config.js
@@ -18,10 +18,13 @@
  *
  */
 
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
-//
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const dotenv = require('dotenv');
+dotenv.config();
+const mnemonic = process.env.MNEMONIC;
+
 // const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+// const mnemonic = fs.readFileSync(".env").toString().trim();
 
 module.exports = {
   /**
@@ -43,9 +46,15 @@ module.exports = {
     //
     development: {
       host: "127.0.0.1",     // Localhost (default: none)
-      port: 8545,            // Standard Ethereum port (default: none)
+      port: 9545,            // Standard Ethereum port (default: none)
       network_id: "*",       // Any network (default: none)
+    },    
+    rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic, process.env.INFURA_URL),
+      network_id: "4",
+      gas: 5500000
     },
+
     // Another network with more advanced options...
     // advanced: {
     // port: 8777,             // Custom port
@@ -57,13 +66,13 @@ module.exports = {
     // },
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
-    // ropsten: {
-    // provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/YOUR-PROJECT-ID`),
-    // network_id: 3,       // Ropsten's id
+    // rinkeby: {
+    // provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/cce0bf36b0a0483b871d403173f9ab50`),
+    // network_id: 4,       // Rinkeby's id
     // gas: 5500000,        // Ropsten has a lower block limit than mainnet
     // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
     // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-    // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    // skipDryRun: false     // Skip dry run before migrations? (default: false for public nets )
     // },
     // Useful for private networks
     // private: {
